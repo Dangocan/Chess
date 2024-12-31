d56b9f09fa2a96269051c39c1260eb9aa6bc2b39
Dangocan <danilogc1999@gmail.com>
2024-12-31 17:07:16 -0300

    refactor: removes old react

---
+++ frontEndNew/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ChessTable from "./components/ChessTable/ChessTable";
+import ChessPiece from "./components/ChessPiece/ChessPiece";
+
+function App() {
+  return (
+    <>
+      <ChessTable />
+      <ChessPiece />
+    </>
+  );
+}
+
+export default App;
